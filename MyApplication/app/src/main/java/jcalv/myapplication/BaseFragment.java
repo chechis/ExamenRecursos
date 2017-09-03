8c7713413ba613cdc9d5ad258e00e0ed56ef6a51
Julio <jcalvarezg92@gmail.com>
2017-09-02 21:28:44 -0600

    segundo
    todo los layouts hechos, solo se lleva el adapter en el java

---
+++ MyApplication/app/src/main/java/jcalv/myapplication/BaseFragment.java
@@ -0,0 +1,10 @@
+package jcalv.myapplication;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jcalv on 2/09/2017.
+ */
+
+public class BaseFragment extends Fragment {
+}
---
+++ MyApplication/app/src/main/java/jcalv/myapplication/adapter/BaseViewPageAdapter.java
@@ -0,0 +1,39 @@
+package jcalv.myapplication.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import jcalv.myapplication.BaseFragment;
+
+/**
+ * Created by jcalv on 2/09/2017.
+ */
+
+public class BaseViewPageAdapter extends FragmentStatePagerAdapter {
+
+    private String [] tabs;
+    private String [] descriptions;
+
+
+    public BaseViewPageAdapter(FragmentManager manager, String[] tabs, String[] descriptions) {
+        super(manager);
+        this.tabs=tabs;
+        this.descriptions=descriptions;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return BaseFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+}
